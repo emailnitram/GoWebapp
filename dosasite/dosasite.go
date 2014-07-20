e85f20fc2aaeeadb33c0408f73e7f0a06dec0a69
Martin Henry <emailnitram@gmail.com>
2014-07-20 11:24:09 -0700

    added templates

--- dosasite/dosasite.go
+++ dosasite/dosasite.go
@@ -2,14 +2,18 @@ package main
 
 import (
   "fmt"
+  "html/template"
   "log"
   "net/http"
   "os"
+  "path"
 )
 
 func main() {
   fs := http.FileServer(http.Dir("public"))
-  http.Handle("/",fs)
+  http.Handle("/public/", http.StripPrefix("/public", fs))
+
+  http.HandleFunc("/", ServeTemplate)
 
   fmt.Println("Listening...")
   err := http.ListenAndServe(GetPort(), nil)
@@ -28,4 +32,33 @@ func GetPort() string {
     fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
   }
   return ":" + port
+}
+
+func ServeTemplate(w http.ResponseWriter, r *http.Request) {
+  lp := path.Join("templates", "layout.html")
+  fp := path.Join("templates", r.URL.Path)
+
+  // Return a 404 if the template doesn't exist
+  info, err := os.Stat(fp)
+  if err != nil {
+    if os.IsNotExist(err) {
+      http.NotFound(w,r)
+      return
+    }
+  }
+
+  // Return a 404 is the request is for a directory
+  if info.IsDir() {
+    http.NotFound(w,r)
+    return
+  }
+
+  templates, _ := template.ParseFiles(lp, fp)
+  if err != nil {
+    fmt.Println(err)
+    http.Error(w, "500 Internal Server Error", 500)
+    return
+  }
+
+  templates.ExecuteTemplate(w, "layout", nil)
 }
