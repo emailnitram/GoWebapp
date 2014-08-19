7dd426696652c072fad74d1b754ffbbf599c1c9a
Martin Henry <emailnitram@gmail.com>
2014-08-18 20:22:15 -0700

    added longestWord

---
+++ coderbyte/longestWord.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+)
+
+func main() {
+  fmt.Println(longestWord("Hello my name is Martin"))
+  fmt.Println(longestWord("San Francisco is sunny in the summer. Sort of..."))
+}
+
+func longestWord(sen string) string {
+  senSlice := strings.Split(sen, " ")
+  longestWord := senSlice[0]
+  for i := range senSlice {
+    if len(senSlice[i]) > len(longestWord) {
+      longestWord = senSlice[i]
+    }
+  }
+  return longestWord
+}
+
